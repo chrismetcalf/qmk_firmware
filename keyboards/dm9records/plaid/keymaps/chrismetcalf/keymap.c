72b7f997701ebec6fd60069bb68519d415bab758
Chris Metcalf <chris@chrismetcalf.net>
2021-10-14 15:45:23 -0700

    Switching from PTT to HELP

--- keyboards/dm9records/plaid/keymaps/chrismetcalf/keymap.c
+++ keyboards/dm9records/plaid/keymaps/chrismetcalf/keymap.c
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * | Ctrl |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  | Enter| QCap |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  | Shift| PTT  |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  | Shift| Help |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  | Omni | Alt  | GUI  | Raise|    Space    | Lower| Clip | OneP | Moom | Lock |
  * `-----------------------------------------------------------------------------------'
@@ -92,7 +92,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT_plaid_grid(
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_ENT,  AC_QCAP,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, AC_PTT,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, AC_HELP,
     KC_ESC,  AC_OMNI, KC_LALT, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   AC_CLIP, AC_PASS, AC_MOOM, AC_LOCK
 ),
 
